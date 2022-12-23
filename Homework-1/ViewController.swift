f3f7cb5dddb6eb1bf183ca34d689e5b6dfee833d
rasit <m.rstkpln@hotmail.com>
2022-12-23 15:14:54 +0300

    Initial Commit

---
+++ Homework-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-1
+//
+//  Created by rasit on 23.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
